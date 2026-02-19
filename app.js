134ab9d86bddb9ebfe0aad3e3a15acc85590d785
Kamron <kamronmirzooo@gmail.com>
2026-02-19 23:52:29 +0500

    feat: CRUD operations

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const res = require("express/lib/response");
 const app = express();
 
 // Connect to MongoDB
+const db = require("./server").db();
 
 // 1 Kirish
 app.use(express.static("public")); // public folderni ochib qoyish holati
@@ -18,11 +19,31 @@ app.set("view engine", "ejs");
 
 // 4 : Routing code
 app.post("/create-item", (req, res) => {
+  console.log("User entered /create-item");
+  const new_reja = req.body.reja;
+  db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
+    if (err) {
+      console.log(rerr);
+      res.end("smth went wrong");
+    } else {
+      res.end("successfully added");
+    }
+  });
   // DB
 });
 
 app.get("/", function (req, res) {
-  res.render("main");
+  console.log("User entered /");
+  db.collection("plans")
+    .find()
+    .toArray((err, data) => {
+      if (err) {
+        console.log(err);
+        res.end("smth went wrong");
+      } else {
+        res.render("main", { items: data });
+      }
+    });
 });
 
 module.exports = app;
